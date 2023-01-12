46f828fd1b8cb4ade1537a796ab3e896056d27a5
Niklas Hedlund <niklas@orisoft.com>
2023-01-12 16:06:23 +0100

    Describe the example app

--- src/NiceCli.Examples.CliDescription/Program.cs
+++ src/NiceCli.Examples.CliDescription/Program.cs
@@ -1,4 +1,11 @@
 ﻿using NiceCli;
 
 return await CliApp.WithArgs(args)
+  .Named("custom-name")
+  .Description("This app only serves as an example really")
+  .Version("1.0-custom-version")
+  .Example("custom-name run")
+  .Example("custom-name migrate")
+  .Example("custom-name export <start-date> <end-date>")
+  .LearnMore("Read about defining the CLI at https://github.com/nhedlund/nicecli")
   .RunAsync();
