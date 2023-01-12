5f9ddbbb030894746143cbd320701e93f63adfa6
Niklas Hedlund <niklas@orisoft.com>
2023-01-12 13:39:08 +0100

    Cleanup dotnet run host example

--- src/NiceCli.Examples.DotnetRunHost/Program.cs
+++ src/NiceCli.Examples.DotnetRunHost/Program.cs
@@ -2,6 +2,8 @@
 using NiceCli;
 using NiceCli.Dotnet;
 
+// CLI app has one default command that runs the host
+
 var cliApp = CliApp.WithArgs(args)
   .CommandRun("Starts stuff", CliDefault.Yes);
 
